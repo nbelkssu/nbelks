e6a6be003475d926eafca0cf13708e3445fbf521
nbelkssu <90729275+nbelkssu@users.noreply.github.com>
2021-09-14 17:59:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-function redirect (url) {
-  var ua        = navigator.userAgent.toLowerCase(),
-      isIE      = ua.indexOf('msie') !== -1,
-      version   = parseInt(ua.substr(4, 2), 10);
-
-  // Internet Explorer 8 and lower
-  if (isIE && version < 9) {
-      var link = document.createElement('a');
-      link.href = url;
-      document.body.appendChild(link);
-      link.click();
-  }
-
-  // All other browsers can use the standard window.location.href (they don't lose HTTP_REFERER like Internet Explorer 8 & lower does)
-  else { 
-      window.location.href = url; 
-  }
+window.onload = function() {
+    location.href = "https://www.elks.org/lodges/ContactUs.cfm?LodgeNumber=2279";
 }
-
-redirect('https://www.elks.org/lodges/ContactUs.cfm?LodgeNumber=2279');
